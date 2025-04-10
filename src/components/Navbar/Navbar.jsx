f74ff4d4afac8391709dd920935f1c4ed75a24bf
unknown <samehrizk1993@gmail.com>
2025-04-10 18:52:45 +0200

    adding assets folder and navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import { assets } from "../../assets/assets";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="logo" />
+    </div>
+  );
+}
+
+export default Navbar;
